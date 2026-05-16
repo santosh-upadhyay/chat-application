69581576a4bb76f95a9f52d4efce04f1a0eb4eb8
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2026-05-17 01:50:41 +0530

    added chat schema

---
+++ server/src/models/chat.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const chatSchema  = new mongoose.Schema({
+
+    participants:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+
+        }],
+        lastMessage:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Message'
+        },
+        unreadMessagesCount:{
+            type:Number,
+            default:0
+        }   
+},{
+    timestamps:true
+})
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+export default Chat;
