a67d1d4d61c2744fd474233e6c7d0f0346a138f6
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2026-05-17 01:05:57 +0530

    autherization middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,3 +1,26 @@
 import jwt from 'jsonwebtoken';
 
 
+const authMiddleware = async (req,res,next) => {
+    try {
+        const token  = req.headers.authorization?.split(" ")[1];
+        if(!token){
+            return res.status(401).json({success:false, message:"Unauthorized"});   
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.findById(decoded._id);
+        if(!user){
+            return res.status(401).json({success:false, message:"Unauthorized"});
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({success:false, message:"Internal server error"});
+    }
+}
+
+
+export default authMiddleware;
