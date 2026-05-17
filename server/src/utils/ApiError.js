6eba40d7c34f1861e8d06f47e22d45acbc2b8add
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2026-05-17 13:54:09 +0530

    added ApiError

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="something went wrong",
+        errors = [],
+        stack
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.data = null;
+        this.success = false;
+
+
+    }
+}
+
+export {ApiError};
